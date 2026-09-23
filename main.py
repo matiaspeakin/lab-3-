b5acc9c9fc6d4e64a97a71bdfe12f1e67abfbbf2
matiaspeakin <mrubio29@setoncatholic.org>
2026-09-23 20:18:20 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# divide function 
+X= int(input("enter first number :"))
+y= int(input("enter second number :"))
+def add(x,y):
+    print(x/y)
+
+# add function
+def add(x,y):
+    print(x+y)
+    
+# multiple funcion
+def multiple(x,y):
+    print(x*y)
