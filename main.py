b0d9fe6a910e90cd11156b6b3e9d9fdde55c528b
matiaspeakin <mrubio29@setoncatholic.org>
2026-09-25 15:56:28 +0000

    final commit

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 # divide function 
-X= int(input("enter first number :"))
-y= int(input("enter second number :"))
-def add(x,y):
+
+def divide(x,y):
     print(x/y)
 
+
 # add function
+
 def add(x,y):
     print(x+y)
+
+
     
 # multiple funcion
 def multiple(x,y):
     print(x*y)
 
+#subtrucation 
+def subtract(x,y):
+    print(x-y)
+
+def get_number(prompt):
+    number = int(input(prompt))
+    if number == 67:
+        print("boi I will slime you out")
+    return number
 
 print ("welcome to the calc short for calulator")
-print ("what we doin")
-print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+while (True):
+    print ("what we doin")
+    print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+    user_choice = input(": ")
+    #print(user_choice)
+
+    if user_choice == 'a':
+        x = get_number("enter first number : ")
+        y = get_number("enter second number : ")
+        add(x,y)
+    elif user_choice == 's':
+        x = get_number("enter first number : ")
+        y = get_number("enter second number : ")
+        subtract (x,y)
+
+    elif user_choice == 'm':
+        x = get_number("enter first number : ")
+        y = get_number("enter second number : ")
+        multiple (x,y)
+
+    elif user_choice == 'd':
+        x = get_number("enter first number : ")
+        y = get_number("enter second number : ")
+        divide (x,y)
+    elif user_choice == 'q':
+        break 
+ 
